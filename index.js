45804121fbf8ec3c90797d64feb8df72fc5ce931
jerichojames1k <jerichojames1k>
2019-09-27 21:13:41 +0800

    Comment

---
+++ index.js
@@ -0,0 +1,62 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+var active = [];// list of active usernames
+var express = require('express');
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+io.on('connection', function (socket) {
+  socket.on('chat_message', function (msg) {
+    io.emit('chat_message', msg);
+    console.log(msg)
+  });
+});
+
+//every connection
+io.on('connection', (socket) => {
+
+
+
+  //default username
+  socket.username = "Anonymous"
+
+
+  //change username
+  socket.on('change_username', (data) => {
+    active.push(data.username)
+    socket.username = data.username;
+    console.log(data.username);
+    io.sockets.emit('change_username', { username: data.username, users: active });
+
+  })
+
+  //new message
+  socket.on('new_message', (data) => {
+    //broadcasting the new message
+    io.sockets.emit('new_message', { message: data.message, username: socket.username });
+  })
+
+  //typing
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', { username: socket.username })
+  })
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+  });
+
+  // socket.on('disconnect', function() {
+  //     console.log('user disconnected')
+  //   });
+});
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
+});
